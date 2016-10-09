61fc63add02b11c37ab8c798e3e440015828b980
Owen Rogers <exortech@gmail.com>
2016-10-08 17:06:02 -0700

    generated faq page with working styles

--- build.js
+++ build.js
@@ -1,19 +1,15 @@
 var Metalsmith  = require('metalsmith');
-var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
-var permalinks  = require('metalsmith-permalinks');
 
 Metalsmith(__dirname)
   .metadata({
   })
   .source('./source')
-  .destination('./build')
-  .clean(true)
-  // .use(markdown())
-  .use(permalinks())
+  .destination('./nic/new/')
+  .clean(false)
   .use(layouts({
     engine: 'handlebars',
-    default: "default.html"
+    default: 'default.html'
   }))
   .build(function(err, files) {
     if (err) { throw err; }
