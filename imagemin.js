7e479774eb1c4f19900b72cfcc6febc7ce24cbc5
Owen Rogers <exortech@gmail.com>
2016-11-25 15:58:12 -0800

    wrap up work on image compression

--- imagemin.js
+++ imagemin.js
@@ -2,11 +2,11 @@ const imagemin = require('imagemin');
 const imageminJpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 
-imagemin(['source/img/**/*.{jpg,png}'], 'public/', {
+imagemin(['**/*.*'], 'public/img', {
 	use: [
 		imageminJpeg(),
 		imageminPngquant()
-	], cwd: "source"
+	], cwd: "source/img"
 }).then(() => {
     console.log('Images optimized');
 });
