805c4a0e5ece69f2b76d2dcb8d227e4a7c94b517
Owen Rogers <exortech@gmail.com>
2017-02-27 13:39:16 -0800

    upgrade stylelint; include svgo in image compression

--- imagemin.js
+++ imagemin.js
@@ -1,12 +1,15 @@
-const imagemin = require('imagemin');
-const imageminJpeg = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
+const imagemin = require('imagemin')
+const imageminJpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const imageminSvgo = require('imagemin-svgo')
 
 imagemin(['**/*.*'], 'public/img', {
-	use: [
-		imageminJpeg(),
-		imageminPngquant()
-	], cwd: "source/img"
+  use: [
+    imageminJpeg(),
+    imageminPngquant(),
+    imageminSvgo()
+  ],
+  cwd: 'source/img'
 }).then(() => {
-    console.log('Images optimized');
-});
+  console.log('Images optimized')
+})
