c321beb258fea50095c12a18fa77b2a61e65a1c4
Owen Rogers <exortech@gmail.com>
2017-06-07 15:19:44 -0700

    fixing imagemin script

--- imagemin.js
+++ imagemin.js
@@ -3,15 +3,14 @@ const imageminJpeg = require('imagemin-mozjpeg')
 const imageminPngquant = require('imagemin-pngquant')
 const imageminSvgo = require('imagemin-svgo')
 
-imagemin(['**/*.*'], 'public/img/', {
+imagemin(['source/img/*.*'], 'public/img/', {
   use: [
     imageminJpeg({
       quality: 40
     }),
     imageminPngquant(),
     imageminSvgo()
-  ],
-  cwd: 'source/img/'
+  ]
 }).then(() => {
   console.log('Images optimized')
 })
