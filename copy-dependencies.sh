4528a1c3852c22f4685fb59dc221b04ea4746101
Owen Rogers <exortech@gmail.com>
2021-11-15 12:31:17 -0800

    add upgraded sentry libraries

--- copy-dependencies.sh
+++ copy-dependencies.sh
@@ -1,15 +1,15 @@
-SENTRY_VERSION=6.13.3
+SENTRY_VERSION=6.14.3
 BOOTSTRAP_VERSION=3.4.1
 BOOTSTRAP_VALIDATOR_VERSION=0.11.9
 
-mkdir -p public/js/sentry/$SENTRY_VERSION/
-cp node_modules/@sentry/browser/build/bundle.min.js* public/js/sentry/$SENTRY_VERSION/
+mkdir -p source/js/sentry/$SENTRY_VERSION/
+cp node_modules/@sentry/browser/build/bundle.min.js* source/js/sentry/$SENTRY_VERSION/
 
-mkdir -p public/js/bootstrap/$BOOTSTRAP_VERSION/
-mkdir -p public/css/bootstrap/$BOOTSTRAP_VERSION/
-mkdir -p public/css/bootstrap/fonts/
-cp node_modules/bootstrap/dist/js/bootstrap.min.js* public/js/bootstrap/$BOOTSTRAP_VERSION/
-cp node_modules/bootstrap/dist/css/bootstrap.min.css* public/css/bootstrap/$BOOTSTRAP_VERSION/
-cp node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.* public/css/bootstrap/fonts/
+mkdir -p source/js/bootstrap/$BOOTSTRAP_VERSION/
+mkdir -p source/css/bootstrap/$BOOTSTRAP_VERSION/
+mkdir -p source/css/bootstrap/fonts/
+cp node_modules/bootstrap/dist/js/bootstrap.min.js* source/js/bootstrap/$BOOTSTRAP_VERSION/
+# cp node_modules/bootstrap/dist/css/bootstrap.min.css* source/css/bootstrap/$BOOTSTRAP_VERSION/
+cp node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.* source/css/bootstrap/fonts/
 
-cp node_modules/bootstrap-validator/dist/validator.min.js public/js/bootstrap-validator-$BOOTSTRAP_VALIDATOR_VERSION.min.js
+cp node_modules/bootstrap-validator/dist/validator.min.js source/js/bootstrap-validator-$BOOTSTRAP_VALIDATOR_VERSION.min.js
