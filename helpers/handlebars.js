233f778958d340fa1ade430a11efc00286dcf07a
Owen Rogers <exortech@gmail.com>
2022-04-20 16:19:29 -0700

    localize header

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,15 +1,12 @@
 const Handlebars = require('handlebars')
 const dateFormat = require('date-fns/format')
 
-Handlebars.registerHelper('equal', function (lvalue, rvalue, options) {
-  if (arguments.length < 3) {
-    throw new Error('Handlebars Helper equal needs 2 parameters')
-  }
-  if (lvalue !== rvalue) {
-    return options.inverse(this)
-  } else {
-    return options.fn(this)
-  }
+Handlebars.registerHelper('equal', function (lvalue, rvalue) {
+  return lvalue === rvalue
+})
+
+Handlebars.registerHelper('notequal', function (lvalue, rvalue) {
+  return lvalue !== rvalue
 })
 
 Handlebars.registerHelper('checkActive', function (current, active) {
