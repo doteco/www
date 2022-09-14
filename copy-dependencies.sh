f527b95786abd310a7bc377016ba398c9648b72b
Owen Rogers <exortech@gmail.com>
2022-09-14 12:13:10 -0700

    upgrade bootstrap; eliminate jquery

--- copy-dependencies.sh
+++ copy-dependencies.sh
@@ -1,19 +1,13 @@
 SENTRY_VERSION=6.19.7
-BOOTSTRAP_VERSION=3.4.1
+BOOTSTRAP_VERSION=5.2.1
 BOOTSTRAP_VALIDATOR_VERSION=0.11.9
-JQUERY_VERSION=3.6.1
-
-cp node_modules/jquery/dist/jquery.min.js source/js/jquery-$JQUERY_VERSION.min.js
-cp node_modules/jquery/dist/jquery.min.map source/js/jquery-$JQUERY_VERSION.min.map
 
 mkdir -p source/js/sentry/$SENTRY_VERSION/
 cp node_modules/@sentry/browser/build/bundle.min.js* source/js/sentry/$SENTRY_VERSION/
 
 mkdir -p source/js/bootstrap/$BOOTSTRAP_VERSION/
 mkdir -p source/css/bootstrap/$BOOTSTRAP_VERSION/
-mkdir -p source/css/bootstrap/fonts/
-cp node_modules/bootstrap/dist/js/bootstrap.min.js* source/js/bootstrap/$BOOTSTRAP_VERSION/
-# cp node_modules/bootstrap/dist/css/bootstrap.min.css* source/css/bootstrap/$BOOTSTRAP_VERSION/
-cp node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.* source/css/bootstrap/fonts/
+cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js* source/js/bootstrap/$BOOTSTRAP_VERSION/
+cp node_modules/bootstrap/dist/css/bootstrap.min.css* source/css/bootstrap/$BOOTSTRAP_VERSION/
 
 cp node_modules/bootstrap-validator/dist/validator.min.js source/js/bootstrap-validator-$BOOTSTRAP_VALIDATOR_VERSION.min.js
