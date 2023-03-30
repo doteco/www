cd0c393975d01309fd6b3cce51207392f5e92002
Owen Rogers <exortech@gmail.com>
2023-03-30 15:25:51 -0700

    remove unused bootstrap-validator

--- copy-dependencies.sh
+++ copy-dependencies.sh
@@ -9,5 +9,3 @@ mkdir -p source/js/bootstrap/$BOOTSTRAP_VERSION/
 mkdir -p source/css/bootstrap/$BOOTSTRAP_VERSION/
 cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js* source/js/bootstrap/$BOOTSTRAP_VERSION/
 cp node_modules/bootstrap/dist/css/bootstrap.min.css* source/css/bootstrap/$BOOTSTRAP_VERSION/
-
-cp node_modules/bootstrap-validator/dist/validator.min.js source/js/bootstrap-validator-$BOOTSTRAP_VALIDATOR_VERSION.min.js
