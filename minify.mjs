59a3ece85e13ead1ccfa1099603a1605d8a0286a
Owen Rogers <exortech@gmail.com>
2023-10-05 14:12:28 -0700

    image minify script

---
+++ minify.mjs
@@ -0,0 +1,26 @@
+import sharp from 'sharp'
+
+const quality = 30
+
+function outputOptions (format) {
+  switch (format) {
+    case 'jpeg':
+      return {
+        quality,
+        mozjpeg: true
+      }
+    default:
+      return { quality }
+  }
+}
+async function minify (inputFile, format = 'webp') {
+  let outputFile = inputFile // inputFile.replace(/^source/, 'public-img')
+  outputFile = outputFile.replace(/\.[^.]+$/, '.' + format)
+  console.log(`Converting ${inputFile} to ${outputFile}`)
+
+  await sharp(inputFile)[format](outputOptions(format)).toFile(outputFile)
+}
+
+const inputFile = process.argv[2]
+const format = process.argv[3]
+await minify(inputFile, format)
