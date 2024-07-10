e086df8744647a49ab1f393951893a2906326887
Owen Rogers <exortech@gmail.com>
2024-07-10 15:01:47 -0700

    fix menu on mobile; missing js file

--- source/js/bootstrap.custom.js
+++ source/js/bootstrap.custom.js
@@ -1,2 +1,3 @@
 export { default as Carousel } from '../../node_modules/bootstrap/js/src/carousel.js'
+export { default as Collapse } from '../../node_modules/bootstrap/js/src/collapse.js'
 export { default as Dropdown } from '../../node_modules/bootstrap/js/src/dropdown.js'
