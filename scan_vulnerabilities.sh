a811c6f024b6da9513107ccfb860d531a672efbd
Owen Rogers <exortech@gmail.com>
2025-11-10 11:32:45 -0800

    upgrade deps

--- scan_vulnerabilities.sh
+++ scan_vulnerabilities.sh
@@ -1,4 +1,4 @@
 syft . -o json > www-sbom.json
-grype sbom:./www-sbom.json > vulnerabilities.txt
+TMPDIR=~/Library/Caches/grype/db/ grype sbom:./www-sbom.json > vulnerabilities.txt
 git diff ./vulnerabilities.txt
 
